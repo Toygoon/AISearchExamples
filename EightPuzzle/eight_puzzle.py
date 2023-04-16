75e3c843370ed32e77b5ee8b42812ff72b052ab2
Toygoon <toygoon@gmail.com>
2023-04-16 15:18:06 +0900

    EightPuzzle: move implementation

--- EightPuzzle/eight_puzzle.py
+++ EightPuzzle/eight_puzzle.py
@@ -8,11 +8,16 @@ class EightPuzzle:
     # Define directions as a class-level variable.
     DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-    def __init__(self, board):
+    def __init__(self, board, func):
         """
         Initialize the game board with the provided board state.
+
+        Args:
+            board (list): The game board state as a 2D list.
+            func (function): The search function to use for solving the puzzle.
         """
         self.board = board
+        self.func = func
 
     def move(self, row, col) -> bool:
         """
@@ -26,7 +31,22 @@ class EightPuzzle:
         Returns:
             bool: True if the tile was successfully moved, False otherwise.
         """
-        pass
+        # Find the location of the empty space
+        empty_row, empty_col = self.find(0)
+        for d in self.DIRS:
+            # Calculate new directions
+            new_row, new_col = row + d[0], col + d[1]
+
+            # Check if the move is valid
+            if new_row == empty_row and new_col == empty_col:
+                # Swap the positions of the tiles
+                self.board[row][col], self.board[empty_row][empty_col] = \
+                    self.board[empty_row][empty_col], self.board[row][col]
+
+                # Return True to indicate a successful move
+                return True
+
+        return False
 
     def solved(self) -> bool:
         """
@@ -36,8 +56,12 @@ class EightPuzzle:
         Returns:
             bool: True if the board is solved, False otherwise.
         """
-        # Solved check implementation
-        pass
+        # The goal state is a 3x3 grid with the numbers 1 to 8 in increasing order,
+        # with a blank space at the bottom-right corner.
+        goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+        # Compare the current board state with the goal state.
+        return self.board == goal
 
     def scramble(self):
         """
